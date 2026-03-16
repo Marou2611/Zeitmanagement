5cb04fba3a16d98cee78f14eb405a75effe8d925
Marou2611 <132606232+Marou2611@users.noreply.github.com>
2026-03-16 23:47:29 +0100

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -3,14 +3,14 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Schedule;
 use Illuminate\Support\Facades\Artisan;
+use App\Http\Controllers\NotificationController;
+use Illuminate\Http\Request;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-
 Schedule::call(function () {
-    $url = config('app.url') . '/notifications/send';
-    $context = stream_context_create(['http' => ['timeout' => 30]]);
-    @file_get_contents($url, false, $context);
+    $controller = new NotificationController();
+    $controller->sendNotification(new Request());
 })->everyMinute();
